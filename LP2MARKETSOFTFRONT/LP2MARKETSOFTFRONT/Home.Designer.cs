6f989dabca6c67a9c565d17515edd60251558372
JorgeRenatoLeon <jrleon@pucp.edu.pe>
2019-10-05 12:50:02 -0500

    Agregar Front

---
+++ LP2MARKETSOFTFRONT/LP2MARKETSOFTFRONT/Home.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace LP2MARKETSOFTFRONT
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(77, 87);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(72, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "TECNOQUIM";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 659);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
---
+++ LP2MARKETSOFTFRONT/LP2MARKETSOFTFRONT/Login.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace LP2MARKETSOFTFRONT
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.lblLinea1 = new System.Windows.Forms.Label();
+            this.lblLinea2 = new System.Windows.Forms.Label();
+            this.lblLinea3 = new System.Windows.Forms.Label();
+            this.lblLinea4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtUsuario.Location = new System.Drawing.Point(326, 222);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(199, 20);
+            this.txtUsuario.TabIndex = 0;
+            this.txtUsuario.Tag = "";
+            this.txtUsuario.Text = "Usuario";
+            this.txtUsuario.TextChanged += new System.EventHandler(this.txtUsuario_TextChanged);
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtContrasena.Location = new System.Drawing.Point(326, 267);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(199, 20);
+            this.txtContrasena.TabIndex = 1;
+            this.txtContrasena.Text = "Password";
+            this.txtContrasena.UseSystemPasswordChar = true;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(150)))), ((int)(((byte)(19)))));
+            this.lblUsuario.Location = new System.Drawing.Point(135, 224);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 3;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(150)))), ((int)(((byte)(19)))));
+            this.lblContrasena.Location = new System.Drawing.Point(135, 270);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(61, 13);
+            this.lblContrasena.TabIndex = 4;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // lblLinea1
+            // 
+            this.lblLinea1.AutoSize = true;
+            this.lblLinea1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(150)))), ((int)(((byte)(19)))));
+            this.lblLinea1.Location = new System.Drawing.Point(237, 224);
+            this.lblLinea1.Name = "lblLinea1";
+            this.lblLinea1.Size = new System.Drawing.Size(64, 13);
+            this.lblLinea1.TabIndex = 5;
+            this.lblLinea1.Text = "-------------------";
+            // 
+            // lblLinea2
+            // 
+            this.lblLinea2.AutoSize = true;
+            this.lblLinea2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(150)))), ((int)(((byte)(19)))));
+            this.lblLinea2.Location = new System.Drawing.Point(237, 270);
+            this.lblLinea2.Name = "lblLinea2";
+            this.lblLinea2.Size = new System.Drawing.Size(64, 13);
+            this.lblLinea2.TabIndex = 6;
+            this.lblLinea2.Text = "-------------------";
+            // 
+            // lblLinea3
+            // 
+            this.lblLinea3.AutoSize = true;
+            this.lblLinea3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(150)))), ((int)(((byte)(19)))));
+            this.lblLinea3.Location = new System.Drawing.Point(543, 224);
+            this.lblLinea3.Name = "lblLinea3";
+            this.lblLinea3.Size = new System.Drawing.Size(64, 13);
+            this.lblLinea3.TabIndex = 7;
+            this.lblLinea3.Text = "-------------------";
+            // 
+            // lblLinea4
+            // 
+            this.lblLinea4.AutoSize = true;
+            this.lblLinea4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(150)))), ((int)(((byte)(19)))));
+            this.lblLinea4.Location = new System.Drawing.Point(543, 270);
+            this.lblLinea4.Name = "lblLinea4";
+            this.lblLinea4.Size = new System.Drawing.Size(64, 13);
+            this.lblLinea4.TabIndex = 8;
+            this.lblLinea4.Text = "-------------------";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LP2MARKETSOFTFRONT.Properties.Resources.Inicio;
+            this.pictureBox1.Location = new System.Drawing.Point(341, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 80);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(341, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(58)))), ((int)(((byte)(103)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblLinea4);
+            this.Controls.Add(this.lblLinea3);
+            this.Controls.Add(this.lblLinea2);
+            this.Controls.Add(this.lblLinea1);
+            this.Controls.Add(this.lblContrasena);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.Label lblLinea1;
+        private System.Windows.Forms.Label lblLinea2;
+        private System.Windows.Forms.Label lblLinea3;
+        private System.Windows.Forms.Label lblLinea4;
+        private System.Windows.Forms.Button button1;
+    }
+}
