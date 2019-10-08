5603164b88cadfda85b3cbb348f19b8a938f0de7
Jorge Leon <jrleon@pucp.edu.pe>
2019-10-07 19:20:01 -0500

    Correcion Nombre Solucion

--- LP2TECNOQUIMFRONT/LP2MARKETSOFTFRONT/frmHome.cs
+++ LP2TECNOQUIMFRONT/LP2MARKETSOFTFRONT/frmHome.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,6 +14,16 @@ namespace LP2MARKETSOFTFRONT
 {
     public partial class frmHome : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
         public frmHome()
         {
             Thread t = new Thread(new ThreadStart(SplashStart));
