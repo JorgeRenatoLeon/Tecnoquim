10b0cb45806078f4b45116dea11edd7073acb501
JorgeRenatoLeon <jrleon@pucp.edu.pe>
2019-10-07 20:35:41 -0500

    "Sincronizar Forms"

--- LP2TECNOQUIMFRONT/LP2TECNOQUIMFRONT/frmHome.cs
+++ LP2TECNOQUIMFRONT/LP2TECNOQUIMFRONT/frmHome.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,6 +14,16 @@ namespace LP2TECNOQUIMFRONT
 {
     public partial class frmHome : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
 
         public frmHome()
         {
@@ -23,6 +34,7 @@ namespace LP2TECNOQUIMFRONT
             Thread.Sleep(5000);
 
             InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
 
             t.Abort();
 
