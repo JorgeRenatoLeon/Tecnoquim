0e713b82b9284da21983e9d09ca9dc93e4a8e2a5
Jorge Renato Leon Chumpitaz <jrleon@pucp.edu.pe>
2019-10-09 17:31:01 -0500

    Front correciones Maximizar Ventana

--- LP2TECNOQUIMFRONT/frmGerente/frmGerente.Designer.cs
+++ LP2TECNOQUIMFRONT/frmGerente/frmGerente.Designer.cs
@@ -206,8 +206,7 @@
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
