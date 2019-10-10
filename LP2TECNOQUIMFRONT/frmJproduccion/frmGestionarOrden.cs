e2533a009849c665a6f7c2a38ec1041e0fcb1556
alulab14 <alulab14@inf.pucp.edu.pe>
2019-10-09 20:05:58 -0500

    Gestionar Orden

---
+++ LP2TECNOQUIMFRONT/frmJproduccion/frmGestionarOrden.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LP2TECNOQUIMFRONT.frmJproduccion
+{
+    public partial class frmGestionarOrden : Form
+    {
+        public frmGestionarOrden()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
