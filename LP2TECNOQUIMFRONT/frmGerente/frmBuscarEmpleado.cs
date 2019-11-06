524cfab2db1098e269086e05ad97db744832c068
alulab14 <alulab14@inf.pucp.edu.pe>
2019-11-05 20:00:09 -0500

    :cat2: c servces

--- LP2TECNOQUIMFRONT/frmGerente/frmBuscarEmpleado.cs
+++ LP2TECNOQUIMFRONT/frmGerente/frmBuscarEmpleado.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LP2TECNOQUIMFRONT.Service;
 
 namespace LP2TECNOQUIMFRONT.frmGerente
 {
     public partial class frmBuscarEmpleado : Form
     {
+        private Service.trabajador trabajadorSeleccionado;
+        Service.ServicioClient DBController = new Service.ServicioClient();
         public frmBuscarEmpleado()
         {
             InitializeComponent();
+            dgvEmpleados.AutoGenerateColumns = false;
+            dgvEmpleados.DataSource = DBController.listarTrabajadores("");
+        }
+
+        public trabajador TrabajadorSeleccionado { get => trabajadorSeleccionado; set => trabajadorSeleccionado = value; }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            TrabajadorSeleccionado = (Service.trabajador)dgvEmpleados.CurrentRow.DataBoundItem;
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            dgvEmpleados.DataSource = DBController.listarTrabajadores(txtNombre.Text);
+        }
+
+        private void dgvEmpleados_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            Service.trabajador empFila = (Service.trabajador)dgvEmpleados.Rows[e.RowIndex].DataBoundItem;
+            dgvEmpleados.Rows[e.RowIndex].Cells["DNI"].Value = empFila.dni;
+            dgvEmpleados.Rows[e.RowIndex].Cells["NombreCompleto"].Value = empFila.nombres + " " + empFila.apellidos;
+            dgvEmpleados.Rows[e.RowIndex].Cells["Rol"].Value = empFila.rol.descripcion;
         }
     }
 }
